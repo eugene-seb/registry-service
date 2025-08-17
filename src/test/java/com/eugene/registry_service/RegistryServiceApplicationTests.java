8c1a6fbc167c355d1a18dc9f81733df30bf03a02
eugene-seb <etoundisebastien@gmail.com>
2025-08-17 03:25:07 +0200

    Refactor and clean up application.properties.

--- src/test/java/com/eugene/registry_service/RegistryServiceApplicationTests.java
+++ src/test/java/com/eugene/registry_service/RegistryServiceApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RegistryServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+class RegistryServiceApplicationTests
+{
+    
+    @Test
+    void contextLoads() {
+        // Nothing to do for now
+    }
+    
 }
